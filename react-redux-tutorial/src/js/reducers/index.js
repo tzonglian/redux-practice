9137b50261f36f484e7587eac503113a42f2f5ca
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2021-03-24 15:08:59 -0400

    Redux-thunk

--- react-redux-tutorial/src/js/reducers/index.js
+++ react-redux-tutorial/src/js/reducers/index.js
@@ -1,5 +1,5 @@
 import { ADD_ARTICLE } from "../constants/action-types";
-
+import { DATA_LOADED } from "../constants/action-types";
 const initialState = {
   articles: [],
 };
@@ -8,6 +8,11 @@ function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload),
+    });
+  }
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload),
     });
   }
   return state;
